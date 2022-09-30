9cd3f7b9f90a81abee8cc3053c33b03825980129
Kristina <ajiaha@gmail.com>
2022-09-30 23:54:22 +0300

    KotProject

--- app/src/main/java/ru/krisproject/kotucheniy2/MainActivity.java
+++ app/src/main/java/ru/krisproject/kotucheniy2/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.krisproject.kotucheniy2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
         button_main_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
             }
         });
     }
