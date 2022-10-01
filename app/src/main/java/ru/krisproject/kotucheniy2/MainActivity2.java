6de861f4e077e674b20fd5f1bac1dd2de7ff2551
Kristina <ajiaha@gmail.com>
2022-10-01 14:24:40 +0300

    KotProject

--- app/src/main/java/ru/krisproject/kotucheniy2/MainActivity2.java
+++ app/src/main/java/ru/krisproject/kotucheniy2/MainActivity2.java
@@ -1,14 +1,28 @@
 package ru.krisproject.kotucheniy2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button level_1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        level_1 = findViewById(R.id.level_1);
+        level_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3_test.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/ru/krisproject/kotucheniy2/MainActivity3_test.java
+++ app/src/main/java/ru/krisproject/kotucheniy2/MainActivity3_test.java
@@ -1,14 +1,29 @@
 package ru.krisproject.kotucheniy2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity3_test extends AppCompatActivity {
 
+    Button back_from_test;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity3_test);
+        back_from_test = findViewById(R.id.button_main_start);
+        back_from_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity3_test.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
