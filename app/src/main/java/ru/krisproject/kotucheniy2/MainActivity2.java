6f2853ef2763e3826fa034f71b7ceef49ba4b704
Kristina <ajiaha@gmail.com>
2022-10-01 16:54:50 +0300

    KotProject

--- app/src/main/java/ru/krisproject/kotucheniy2/MainActivity2.java
+++ app/src/main/java/ru/krisproject/kotucheniy2/MainActivity2.java
@@ -3,26 +3,26 @@ package ru.krisproject.kotucheniy2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    Button level_1;
+    ImageButton level_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
         level_1 = findViewById(R.id.level_1);
         level_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity2.this, MainActivity3_test.class);
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
                 startActivity(intent);
             }
         });
-
     }
 }
--- app/src/main/java/ru/krisproject/kotucheniy2/MainActivity3_test.java
+++ app/src/main/java/ru/krisproject/kotucheniy2/MainActivity3_test.java
@@ -9,12 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity3_test extends AppCompatActivity {
 
-    Button back_from_test;
+    private Button back_from_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_activity3_test);
+        setContentView(R.layout.activity_main3_test);
+
         back_from_test = findViewById(R.id.button_main_start);
         back_from_test.setOnClickListener(new View.OnClickListener() {
             @Override
